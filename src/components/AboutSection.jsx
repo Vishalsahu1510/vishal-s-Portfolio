039ebaf36fa6cb2a7da6c64b5f27b941d91d4568
Vishal Sahu <vishalsahu0644@gmail.com>
2025-07-10 17:26:20 +0530

    add resume

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -36,7 +36,7 @@ const AboutSection = () => {
               </a>
               
               <a
-                href="/ARYAN_CV (2).pdf"
+                href="/Vishal_Sahu_Resume.pdf"
                 download
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
